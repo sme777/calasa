fdb5611da13dd48f226b7dcefd657bd9c7a818a0
sme777 <samson.petrosyan8@gmail.com>
2022-05-19 20:14:19 -0700

    change aws region

--- config/initializers/aws-sdk.rb
+++ config/initializers/aws-sdk.rb
@@ -1,7 +1,7 @@
 require 'aws-sdk-core'
 
 Aws.config.update(
-  region: 'us-west-2',
+  region: 'us-east-2',
   credentials: Aws::Credentials.new(Rails.application.credentials.config[:S3_KEY], 
                 Rails.application.credentials.config[:S3_SECRET_KEY])
 )
